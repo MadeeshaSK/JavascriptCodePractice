18b26233cb3ff45215ee3a98b200f06b6608ee7e
MadeeshaSK <madeeshasachindu2@gmail.com>
2024-12-05 01:14:18 +0530

    variables and data types

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-hi 
+// use let or var or const to declare a variable
+/*
+string
+    let name = 'John';
+    let name = "John Doe";
+    print to console
+        console.log('Hello World');
+        console.log(typeof address);
+
+number
+    let age = 30;
+    let age = 30.5;
+    let age = -30;
+    console.log(typeof age);
+
+boolean
+    let isMarried = true;
+    let isMarried = false;
+    console.log(isMarried);
+    console.log(typeof isMarried);
+
+null
+    let car = null;
+    console.log(car);
+    console.log(typeof car);
+
+undefined
+    let test;
+    same as, let test = undefined;
+    console.log(test);
+    console.log(typeof test);
+
+symbol
+    let sym = Symbol();
+    console.log(sym);
+    console.log(typeof sym);
+
+object
+    let address = {
+        city: 'Jakarta',
+        country: 'Indonesia'
+    };
+    console.log(address);
+    console.log(typeof address);
+
+array
+    let hobbies = ['music', 'movie', 'reading'];
+    console.log(hobbies);
+    console.log(typeof hobbies);
+
+function
+    let greet = function() {
+        return 'Hello';
+    }
+    console.log(greet);
+    console.log(typeof greet);
+
+*/
