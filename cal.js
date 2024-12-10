d5cd8f6512e5fc25ece440d1c209b10053016119
MadeeshaSK <madeeshasachindu2@gmail.com>
2024-12-10 14:50:15 +0530

    make calculator

---
+++ cal.js
@@ -0,0 +1,31 @@
+let inputElementObj = document.getElementById('inputNumber');
+let numberOne;
+let numberTwo;
+let number1Obj = document.getElementById('number1');
+let number2Obj = document.getElementById('number2');
+
+inputElementObj.addEventListener('keyup', (event)=>{
+    if (event.key == 'Enter'){
+        let tempValue = inputElementObj.value;
+        //check whhether number or not
+        if (!isNaN(tempValue)){
+            numberOne = parseInt(tempValue);
+            inputElementObj.textContent = '';
+            number1Obj.textContent = numberOne;
+        }
+    } 
+});
+
+inputElementObj.addEventListener('keyup', (event)=>{
+    if (event.key == 'Enter'){
+        let tempValue = inputElementObj.value;
+        //check whhether number or not
+        if (!isNaN(tempValue)){
+            numberTwo = parseInt(tempValue);
+            inputElementObj.textContent = '';
+            number2Obj.textContent = numberTwo;
+        }
+    } 
+});
+
+addEventListener
--- main.js
+++ main.js
@@ -15,6 +15,21 @@ form.addEventListener('submit', function(event){
     form.reset();
 })  
 
+// trigger when scroll
+
+window.addEventListener('scroll',()=>{
+    if (window.pageYOffset > 100) {
+        document.getElementById('header')
+        .style.backgroundColor = 'red';
+    }else {
+        document.getElementById('header')
+        .style.backgroundColor = 'aqua';
+    }
+})
+
+
+
+
 // change text
 
 // let head1 = document.getElementById('heading1');
@@ -52,7 +67,7 @@ head1.style.color = 'red'; //DOM property
 
 // hide/show
 
-let sh = document.getElementsByTagName('div')[1];
+let sh = document.getElementsByTagName('div')[2];
 let btn3 = document.getElementById('sh-d');
 function showAndHide(){
     if (sh.style.display === 'none') {
@@ -77,4 +92,121 @@ hvr1.addEventListener('mousemove',(e)=>{
     let x = e.clientX;
     let y = e.clientY;
     console.log(x,y);
+})
+
+//add/remove content
+
+let ulList = document.getElementById('ul-list');
+let counter = 1;
+document.getElementById('btn-add')
+.addEventListener('click',()=>{
+    let li = document.createElement('li');
+    li.textContent = 'New Item'+ counter;
+    ulList.appendChild(li);
+    counter++;
+})
+document.getElementById('btn-remove')
+.addEventListener('click',()=>{
+    let temp = ulList.getElementsByTagName('li');
+    if (temp.length > 0) {
+        ulList.removeChild(temp[temp.length-1]);
+    }
+    counter--;
+})
+
+
+// see what type
+
+let inputElement = document.getElementById('input-element');
+inputElement.addEventListener('keyup',(e)=>{
+    console.log(e.target.value);
+})
+
+//bubeling
+//avoid event propagation
+// when click btn all 3 triger, when click inner, inner and outer trigger
+let outerDiv = document.getElementById('outer');
+let innerDiv = document.getElementById('inner');
+let btnInner = document.getElementById('btn-inner');
+
+outerDiv.addEventListener('click',()=>{console.log('Outer Div')});
+innerDiv.addEventListener('click',(e)=>{console.log('Inner Div');e.stopPropagation();});
+btnInner.addEventListener('click',(e)=>{console.log('Btn Inner');e.stopPropagation();});
+
+
+
+
+// <!-- callback -->
+
+/*pass a function as an argument to another function
+if we want to trigger a codeblock after the completion of another codeblock,
+ which is taking time to execute*/
+
+/*
+let product;
+function orderMyPizza(){
+    console.log('i am hungry');
+    console.log('i am ordering ');
+    setTimeout(()=>{
+        product = 'Pizza';
+    },3000)
+}
+
+orderMyPizza();
+console.log('I am having my ' + product);
+console.log('I am waiting ');
+*/
+
+/*
+output
+i am hungry
+i am ordering 
+I am having my undefined
+I am waiting
+*/
+let product;
+function orderMyPizza(callbackFunction){
+    console.log('i am hungry');
+    console.log('i am ordering ');
+    setTimeout(()=>{
+        product = 'Pizza';
+        callbackFunction(product);
+    },3000)
+}
+function complete(productName){
+    console.log('I am having my ' + productName);
+}
+orderMyPizza(complete);
+console.log('I am waiting ');
+
+// http request
+
+console.log('Start');
+let comments ;
+//create a new instance of XMLHttpRequest
+let http1 = new XMLHttpRequest();
+//initialize the request
+http1.open('GET','https://jsonplaceholder.typicode.com/comments');
+//send the request
+http1.send();
+//listen for the response
+function loadData(printData){
+    http1.onreadystatechange = function(){
+        if (http1.readyState === 4 && http1.status === 200) {
+            comments = JSON.parse(http1.responseText);
+            printData();
+        }
+    }
+}
+function printData(){
+    console.log(comments);
+}
+loadData(printData);
+console.log('End');
+
+//when we refresh without saving, show alert
+
+window.addEventListener('beforeunload', (event)=>{
+    event.preventDefault();
+    event.returnValue('are you crazy?');
 })
