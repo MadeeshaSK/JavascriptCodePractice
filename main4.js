7c57c06e131c6512adb82eadd1d13f1c03348341
MadeeshaSK <madeeshasachindu2@gmail.com>
2025-03-04 15:30:35 +0530

    Geolocation API, design patterns, axios, flatMap

--- main4.js
+++ main4.js
@@ -105,3 +105,126 @@ Person2.prototype.sayHello =()=>{
 }
 console.log(Kamal.gender);
 Kamal.sayHello();
+
+// Geolocation API
+/*
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((location) => {
+        const longitude = location.coords.longitude;
+        const latitude = location.coords.latitude;
+        console.log(longitude, latitude);
+
+        const mapData =new google.maps.Map(document.getElementById('map-context'), {
+            center: {lat: latitude, lng: longitude},
+            zoom: 8
+        });
+
+        new google.maps.Marker({
+            position: {lat: latitude, lng: longitude},
+            map: mapData,
+            title: 'My Location'
+        });
+    })
+} else {
+    console.log('Geolocation is not supported');
+}
+*/
+
+// design patterns
+
+// 1. Singleton
+// js clauses are singleton by default
+const Singleton =( () => {
+    let instance;
+    const createInstance =()=> {
+        return new Object();
+    }
+    return {
+        getInstance: () => {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    }
+})();
+const i1 = Singleton.getInstance();
+const i2 = Singleton.getInstance();
+console.log(i1 === i2);
+
+//2. Factory
+class Customer {
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+}
+class Factory {
+    createCustomer(name, address) {
+        return new Customer(name, address);
+    }
+}
+const factory = new Factory();
+const c1 = factory.createCustomer('Nimal', 'Colombo');
+const c2 = factory.createCustomer('Amlal', 'Kandy');
+console.log(c1);
+console.log(c2);
+
+//3. Observer
+
+class Subject{
+    constructor() {
+        this.observers =[];
+    }
+    Substribe(observer){
+        this.observers.push(observer);
+    }
+    Unsubstribe(observer){
+        this.observers = this.observers.filter((obs) => obs !== observer);
+    }
+    notify(message) {
+        this.observers.forEach((observer) => observer.showMessage(message));
+    }
+}
+ class Observer{
+    constructor(username) {
+        this.username = username;
+    }
+    showMessage(message){
+        console.log(`${this.username} received message: ${message}`);
+    }
+ }
+    const subject = new Subject();
+    const observer1 = new Observer('Nimal');
+    const observer2 = new Observer('Kamal');
+    subject.Substribe(observer1);
+    subject.Substribe(observer2);
+    subject.notify('Hello');
+
+
+    // axios
+// axios use for http requests in js (get, post, put, delete)
+// axios is a promise based library
+// axios is a third party library
+
+/*
+axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(dataResponse => {
+        console.log(dataResponse.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+*/
+
+// flatMap
+// flatmap is a combination of map and flat
+// flatmap is used to flatten the array
+// flatmap is used to transform the array
+
+
+const dataArray = [1,2,3,4,5];
+const finalizedData = dataArray.flatMap((data) => [data * 2]);
+console.log(finalizedData);
+const finalizedData2 = dataArray.findIndex(data=>data>2);
+console.log(finalizedData2);
